4b9321b53c3d40cd2a61ed2bf6cd501b5247fad6
Lucien Najev <luciennajev@hotmail.com>
2018-09-07 22:22:52 +0100

    added weather API

--- interface.js
+++ interface.js
@@ -28,9 +28,38 @@ $(document).ready(function() {
     $('#power-saving-status').text('off');
   });
 
+
+
+  // call to the weatherAPI
+  displayWeather('London');
+
+  $('#current-city').change(function() {
+    var city = $('#current-city').val();
+    displayWeather(city);
+  });
+  // $('#current-city').change(function() {
+  //   // displayWeather('Paris');
+  // })
+
+
+
+
+
+
+
   function updateTemperature() {
     $('#temperature').text(thermostat.temperature);
     $('#temperature').attr('class', thermostat.energyUsage());
+  };
+
+
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q='+ city;
+    var units = '&units=metric';
+    var token = '&appid=3d1b4735c79467224f1d7ed6725b41fc';
+    $.get(url + units + token, function(data) {
+      $('#current-temperature').text(data.main.temp);
+    });
   };
 
 });
