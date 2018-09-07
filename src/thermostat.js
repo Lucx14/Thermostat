74fa71d1131be5cf4c3783b7aa5cf0016c44780f
Lucien Najev <luciennajev@hotmail.com>
2018-09-07 12:51:48 +0100

    functionality added

--- src/thermostat.js
+++ src/thermostat.js
@@ -2,7 +2,10 @@
 
 function Thermostat() {
   this.MINIMUM_TEMPERATURE = 10;
+  this.MAX_LIMIT_PSM_ON = 25;
+  this.MAX_LIMIT_PSM_OFF = 32;
   this.temperature = 20;
+  this.powerSavingMode = true;
 };
 
 
@@ -12,6 +15,9 @@ Thermostat.prototype.getCurrentTemperature = function() {
 
 
 Thermostat.prototype.up = function() {
+  if(this.isMaximumTemperature()) {
+    return;
+  };
   this.temperature += 1;
 };
 
@@ -24,4 +30,23 @@ Thermostat.prototype.down = function() {
 
 Thermostat.prototype.isMinimumTemperature = function() {
   return this.temperature === this.MINIMUM_TEMPERATURE;
+};
+
+Thermostat.prototype.isPowerSavingModeOn = function() {
+  return this.powerSavingMode === true;
+};
+
+Thermostat.prototype.switchPowerSavingModeOff = function() {
+  this.powerSavingMode = false;
+};
+
+Thermostat.prototype.switchPowerSavingModeOn = function() {
+  this.powerSavingMode = true;
+};
+
+Thermostat.prototype.isMaximumTemperature = function() {
+  if (this.isPowerSavingModeOn() === false) {
+    return this.temperature === this.MAX_LIMIT_PSM_OFF;
+  };
+  return this.temperature === this.MAX_LIMIT_PSM_ON;
 };
